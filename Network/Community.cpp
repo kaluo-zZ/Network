cb9f7592879c3bc966c97d9d53b3c32d6b2ce224
kaluo_zZ <2227241412@qq.com>
2017-04-24 22:34:14 +0800

    将域间业务也放入到并行阶段运行
    运行结果比上次慢

--- Network/Community.cpp
+++ Network/Community.cpp
@@ -1,18 +1,3 @@
-// File: community.h
-// -- community detection source file
-//-----------------------------------------------------------------------------
-// Community detection
-// Based on the article "Fast unfolding of community hierarchies in large networks"
-// Copyright (C) 2008 V. Blondel, J.-L. Guillaume, R. Lambiotte, E. Lefebvre
-//
-// This program must not be distributed without agreement of the above mentionned authors.
-//-----------------------------------------------------------------------------
-// Author   : E. Lefebvre, adapted by J.-L. Guillaume
-// Email    : jean-loup.guillaume@lip6.fr
-// Location : Paris, France
-// Time	    : February 2008
-//-----------------------------------------------------------------------------
-// see readme.txt for more details
 #include "stdafx.h"
 #include "Community.h"
 
--- Network/Main.cpp
+++ Network/Main.cpp
@@ -16,15 +16,18 @@
 #include <thread>
 #include <time.h>
 
-
 using namespace std;
+typedef struct Path_Node{
+	int NodeID;
+	Path_Node* next;
+}Path_Node;
 
 typedef struct Demand{
 	int DemandID;
 	int SourceID;
 	int DestinationID;
 	double Bandwidth;
-
+	Path_Node* next; // 记录业务的路由
 }Demand;
 
 double precision = 0.000001;
@@ -62,25 +65,50 @@ vector<string> split(string str, string pattern)
 }
 
 
-void print(Network* graph, int s, int d, double bandwidth){
+void print(Network* graph, Demand* demand){
+	int s = demand->SourceID;
+	int d = demand->DestinationID;
+	double bandwidth = demand->Bandwidth;
 	Node* no = graph->get_Node(s, ID);
 	Node* des = graph->get_Node(d, ID);
+
 	//cout << "起点" << s << "，终点" << d << "带宽" << bandwidth << ",路径为：";
 	Edge* e = NULL;
+	demand->next = NULL;
 	if (no->pre == NULL){
-		//cout << "NULL" << endl;
-		outfile << "NULL" << endl;
 		return;
 	}
 	while (no->ID != des->ID){
-		//cout << no->ID << "-->";
-		outfile << no->ID << ",";
+		Path_Node* pn = new Path_Node();
+		pn->next = NULL;
+		pn->NodeID = no->ID;
+		if (demand->next == NULL){ // 插入第一个路由节点
+			demand->next = pn;
+		}
+		else{
+			Path_Node* temp = demand->next;
+			while (temp->next != NULL){ // 找到最后一个节点
+				temp = temp->next;
+			}
+			temp->next = pn;
+		}
 		e = graph->get_Edge(no->ID, no->pre->ID);
 		e->availableBandwidth = e->availableBandwidth - bandwidth;
 		no = no->pre;
 	}
-	//cout << des->ID << endl;
-	outfile << des->ID << endl;
+	Path_Node* pn = new Path_Node();
+	pn->next = NULL;
+	pn->NodeID = des->ID;
+	if (demand->next == NULL){ // 插入第一个路由节点
+		demand->next = pn;
+	}
+	else{
+		Path_Node* temp = demand->next;
+		while (temp->next != NULL){ // 找到最后一个节点
+			temp = temp->next;
+		}
+		temp->next = pn;
+	}
 }
 
 void modify_comm_idx(vector<Community*> comms,Network* network, vector<int> v, int level, int comm_idx){
@@ -100,15 +128,15 @@ void modify_comm_idx(vector<Community*> comms,Network* network, vector<int> v, i
 	}
 }
 
-void run_demands(Network* network, vector<Demand*> demand, vector<Demand*> outter_demand){
+void run_demands(Network* network, vector<Demand*> demand){
 	for (int i = 0; i < demand.size(); i++){
 		network->CSPF(demand[i]->SourceID, demand[i]->DestinationID, demand[i]->Bandwidth);
 		Node* no = network->get_Node(demand[i]->SourceID, ID);
-		if (no->pre == NULL){
-			outter_demand.push_back(demand[i]);
-		}
+		
+		if (no->pre == NULL)
+			demand[i]->next = NULL;
 		else
-			print(network, demand[i]->SourceID, demand[i]->DestinationID, demand[i]->Bandwidth);
+			print(network, demand[i]);
 	}
 }
 
@@ -142,14 +170,19 @@ void  quickSort(Demand** pointer_to_demand, int p, int r){
 int _tmain(int argc, _TCHAR* argv[])
 {
 	//_CrtSetBreakAlloc(3575816);
-	ifstream file("dc_topo_new7.csv");
-	ifstream demand_file("dc_demand7.csv");
-	outfile.open("dc_demand7_path.txt");
+	ifstream file("dc_topo_new0.csv");
+	ifstream demand_file("dc_demand0.csv");
+	outfile.open("dc_demand0_path.txt");
 	string value;
 	vector<string> vec;
 	Network* network = new Network();
 	Edge* edge;
 	vector<Community*> communities;
+	const int demand_tot = 20000;
+
+	/*
+		输入模块
+	*/
 
 	// 导入网络拓扑
 	getline(file, value); //去掉表头
@@ -169,8 +202,8 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	// 导入业务集
 	getline(demand_file, value);
-	Demand* demands = (Demand*)malloc(20000 * sizeof(Demand));
-	for (int i = 0; i < 10000; i++){
+	Demand* demands = (Demand*)malloc(demand_tot * sizeof(Demand));
+	for (int i = 0; i < demand_tot; i++){
 		getline(demand_file, value);
 
 		vec = split(value, ",");
@@ -183,10 +216,13 @@ int _tmain(int argc, _TCHAR* argv[])
 		demands[i].SourceID = s;
 		demands[i].DestinationID = d;
 		demands[i].Bandwidth = bandwidth;
+		demands[i].next = NULL;
 	}
 
 	
-	// 进行网络社团划分
+	/*
+		分割模块
+	*/
 
 	Graph*graph = new Graph(network);
 
@@ -233,14 +269,14 @@ int _tmain(int argc, _TCHAR* argv[])
 
 
 	int len = communities.size();
-	int size = communities[len - 1]->comm_nodes.size();
+	int size = communities[len - 1]->comm_nodes.size(); // 分割网络后的社团数目
 	for (int i = 0; i < size;i++){
 		vector<int> v = communities[len - 1]->comm_nodes[i];
 		modify_comm_idx(communities, network, v, len - 2, i);
 	}
 
 	
-	vector<Network*> networks(size);  // 子图
+	vector<Network*> networks(size + 1);  // 子图，前size个储存子图，最后一个存储原图
 	vector<vector<Demand*>> classified_demands(size + 1); // 子图对应的域内业务，最后一个存储域间业务
 	// 输出划分的社团及其包含节点数目
 	vector<int> count(size, 0);
@@ -257,8 +293,11 @@ int _tmain(int argc, _TCHAR* argv[])
 	for (int i = 0; i < size; i++){
 		networks[i] = new Network();
 	}
+	networks[size] = network; // 将原图指针储存在networks最后一个
 
-	
+	/*
+		并行模块
+	*/
 
 	// 为子图分配边，当边的起点和终点在同一社团时，加入这个子图
 	for (int i = 0; i < network->getSumofNodes(); i++){
@@ -278,7 +317,7 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	clock_t start = clock();
 	//为每个子图分配业务
-	for (int i = 0; i < 10000; i++){
+	for (int i = 0; i < demand_tot; i++){
 		int c1 = network->get_Node(demands[i].SourceID, ID)->community;
 		int c2 = network->get_Node(demands[i].DestinationID, ID)->community;
 		if (c1 == c2){ // 域内业务
@@ -310,36 +349,71 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	cout << "计算路由中..." << endl;
 	
-	for (int i = 0; i < size; i++){
-		thread t(run_demands, networks[i], classified_demands[i], classified_demands[size]);
-		//thread t(run_demands, networks[i], pointer_to_demand[i], classified_demands[size]);
+	for (int i = 0; i < size + 1; i++){ // 并行处理业务，需要注意的是链路带宽可能过载，所以后面需要卸载过载链路，进行重路由
+		thread t(run_demands, networks[i], classified_demands[i]);
+		//thread t(run_demands, networks[i], pointer_to_demand[i]);
 		t.join();
 	}
 	
-	
+	/*
+		校正模块
+	*/
+	cout << "...." << endl;
+	vector<Demand*> re_route_demands; // 存储需要重路由的业务
 	// 将子网络的带宽使用情况反映在原网络中
-	for (int i = 0; i < size; i++){
-		for (int j = 0; j < networks[i]->getSumofNodes(); j++){
-			Node* n = networks[i]->get_Node(j, INDEX);
-			Edge* e = n->out;
-			while (e != NULL){
-				Edge* edge = network->get_Edge(e->Source->ID, e->Destination->ID);
-				edge->availableBandwidth = e->availableBandwidth;
-				e = e->right;
+	for (int i = demand_tot - 1; i >= 0; i--){
+		if (demands[i].next == NULL){ 
+			re_route_demands.push_back(demands + i);
+			continue; 
+		}
+		Path_Node* pn = demands[i].next;
+		bool flag = false; // 过载标志
+		while (pn->next != NULL){
+			Path_Node* tp = pn->next;
+			Edge* e = network->get_Edge(pn->NodeID, tp->NodeID);
+			if (demands[i].Bandwidth > e->availableBandwidth){// 这条链路过载了
+				flag = true;
+				break; 
+			}
+			pn = tp;
+		}
+		if (flag){
+			// 处理造成过载的业务
+			re_route_demands.push_back(demands + i);
+		}
+		else{
+			pn = demands[i].next;
+			while (pn->next != NULL){
+				Path_Node* tp = pn->next;
+				Edge* e = network->get_Edge(pn->NodeID, tp->NodeID);
+				e->availableBandwidth = e->availableBandwidth - demands[i].Bandwidth;
+				pn = tp;
 			}
 		}
 	}
 	
-	// 计算域间业务及对上一次求域内路由不成功的业务进行重路由
-	vector<Demand*> ds = classified_demands[size];
-	for (int i = 0; i < ds.size(); i++){
-		network->CSPF(ds[i]->SourceID, ds[i]->DestinationID, ds[i]->Bandwidth);
-		print(network, ds[i]->SourceID, ds[i]->DestinationID, ds[i]->Bandwidth);
+	// 重路由
+	for (int i = 0; i < re_route_demands.size(); i++){
+		network->CSPF(re_route_demands[i]->SourceID, re_route_demands[i]->DestinationID, re_route_demands[i]->Bandwidth);
+		print(network, re_route_demands[i]);
 	}
 	
+	for (int i = 0; i < demand_tot; i++){ // 输出到文件
+		Path_Node* pn = demands[i].next;
+		if (pn == NULL){
+			outfile << "NULL" << endl;
+			continue;
+		}
+		while (pn->next != NULL){
+			outfile << pn->NodeID << ",";
+			pn = pn->next;
+		}
+		outfile << pn->NodeID << endl;
+	}
+
 	clock_t end = clock();
-	cout << "时间：" << static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000 << "ms" << endl;
-	cout << "路由计算结束" << endl;
+	std::cout << "时间：" << static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000 << "ms" << endl;
+	std::cout << "路由计算结束" << endl;
 	
 	/*
 	ofstream ofs("2000.csv");
@@ -368,7 +442,18 @@ int _tmain(int argc, _TCHAR* argv[])
 	for (int i = 0; i < size; i++)
 		delete networks[i];
 	delete network;
-	free(demands);
+	for (int i = 0; i < demand_tot; i++){
+		Path_Node* pn = demands[i].next;
+		if (pn == NULL) continue;
+		while (pn->next != NULL){
+			Path_Node* tp = pn->next;
+			delete pn;
+			pn = tp;
+		}
+		delete pn;
+		pn = NULL;
+	}
+	std::free(demands);
 
 	file.close();
 	demand_file.close();
